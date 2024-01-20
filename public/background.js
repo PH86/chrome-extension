723b27dad8fc3cf749caeea87d5b18ecd51764fe
ph86 <peter.hodgson2011@gmail.com>
2024-01-20 14:20:35 +0000

    Inject div into page

--- public/background.js
+++ public/background.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-undef */
+
 chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.tabs.executeScript({
-    code: `console.log("Extension is working");`,
-  });
+  chrome.tabs.executeScript(null, { file: "/static/js/extension.js" });
 });
